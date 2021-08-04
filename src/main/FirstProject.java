0fce2630b5f1e3b2d8c172148fc534367c38afaa
leilanitaylor <leilani.h.taylor@gmail.com>
2021-08-04 13:22:27 -0600

    - Updated number of times to execute helloWorld method

--- src/main/FirstProject.java
+++ src/main/FirstProject.java
@@ -2,7 +2,7 @@ package main;
 
 public class FirstProject {
     public static void main(String[] args) {
-        FirstProject.helloWorld(4);
+        FirstProject.helloWorld(7);
     }
 
     public static void helloWorld(int x) {
